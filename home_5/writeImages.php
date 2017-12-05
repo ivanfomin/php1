b2e2f3dc63c04b4c7361ed48182c0b19ff3644e8
Ivan Fomin <la-verne@mail.ru>
2017-12-05 17:57:31 +0300

    Update writeImages.php

--- home_5/writeImages.php
+++ home_5/writeImages.php
@@ -4,12 +4,9 @@ include_once "functions.php";
 
 if (isset($_FILES['myFile'])) {
     if (0 == $_FILES['myFile']['error']) {
-        if (strcasecmp(substr($_FILES['myFile']['name'], -4), '.png') ||
-            strcasecmp(substr($_FILES['myFile']['name'], -4), '.jpg')) {
-            move_uploaded_file($_FILES['myFile']['tmp_name'],
-                __DIR__ . '/images/' . $_FILES['myFile']['name']
-            );
-        }
+          move_uploaded_file($_FILES['myFile']['tmp_name'],
+              __DIR__ . '/images/' . $_FILES['myFile']['name']
+          );
     }
 }
 
