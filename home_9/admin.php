43b9e91d87296552b76252d612d5a0b654a227d6
Ivan Fomin <la-verne@mail.ru>
2017-12-14 16:42:30 +0300

    HomeWork_9

--- home_9/admin.php
+++ home_9/admin.php
@@ -3,11 +3,10 @@ require __DIR__ . '/autoload.php';
 
 $book = new \App\Models\GuestBook();
 $records = $book->getRecords();
-$about = new \App\Models\About();
 $image = new \App\Models\Image();
 $images = $image->getImages();
 
-$view = new \App\Views\View($records, $images, $about);
+$view = new \App\Views\View($records, $images);
 $view->display('adminPanel.php');
 
 
